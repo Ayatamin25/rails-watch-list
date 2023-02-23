0baf509ab7709508e28caf8198633326da5b6f17
Ayat Amin <ayat.jafar.amin@gmail.com>
2023-02-23 13:55:19 -0500

    controller of bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :lists, only: %i[index show new create]
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: :destroy
 end
 
 # A user can see all the lists
@@ -9,3 +12,11 @@ end
 # A user can create a new list
 # GET "lists/new"
 # POST "lists"
+# -----------------------------
+# A user can add a new bookmark (movie/list pair) to an existing list
+# Checkout simple_form docs about f.association
+# to easily create a select dropdown for our list of movies.
+# GET "lists/42/bookmarks/new"
+# POST "lists/42/bookmarks"
+# A user can delete a bookmark from a list. How can we make a delete link again?
+# DELETE "bookmarks/25"
