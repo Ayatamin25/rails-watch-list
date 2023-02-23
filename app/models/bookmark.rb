eeec57a1a2d0d1aae211edcf0ff8fb81e955c10c
Ayat Amin <ayat.jafar.amin@gmail.com>
2023-02-23 12:11:26 -0500

    validations and associations

---
+++ app/models/bookmark.rb
@@ -0,0 +1,9 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+  # The comment of a bookmark cannot be shorter than 6 characters.
+  validates :comment, length: { minimum: 6 }
+  validates_uniqueness_of :movie_id, scope: :list_id
+end
+# A bookmark belongs to a movie
+# A bookmark belongs to a list
